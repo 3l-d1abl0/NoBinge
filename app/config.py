d97bc8988674d5e0909993af83e6c46420e54148
Sameer <sameer.barha12@gmail.com>
2025-04-05 13:38:53 +0530

    configs setup done

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
-import logging
 import os
+import yaml
 from functools import lru_cache
 from pydantic_settings import BaseSettings
+import threading
+
 
 from logger import logger
 
@@ -11,8 +13,36 @@ class Settings(BaseSettings):
     testing: bool = os.getenv("testing", False)
     GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "GEMINI_API_KEY")
 
+    # Model Configuration
+    embed_model: str =  os.getenv("embed_model")
+    # Interval between frames in seconds
+    frame_interval: int = os.getenv("frame_interval")
+
+    # Source
+    indexing_path: str =  os.getenv("indexing_path")
+    data_dir: str =  os.getenv("data_dir")
+    video_dir: str =  os.getenv("video_dir")
+
+    # Retrieval Configuration
+    similarity_top_k: int =  os.getenv("similarity_top_k")
+    image_similarity_top_k: int =  os.getenv("image_similarity_top_k")
+
+
+    # Video Processing Configuration
+    # Maximum number of frames to extract
+    max_frames: int =  os.getenv("max_frames")
+    # Minimum segment length in seconds
+    min_segment_length: int =  os.getenv("min_segment_length")
+    # Maximum segment length in seconds
+    max_segment_length: int =  os.getenv("max_segment_length")
+
+    # UI Configuration
+    # Maximum number of frames to display in UI
+    max_display_frames: int =  os.getenv("max_display_frames")
+    frame_display_columns: int =  os.getenv("frame_display_columns")
+
 
 @lru_cache()
-def get_settings() -> BaseSettings:
+def get_envs() -> BaseSettings:
     logger.info("Loading config settings from the environment...")
     return Settings()
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,9 +1,26 @@
-from config import get_settings, Settings
+from config import get_envs, Settings
+#from config import Envs
+
 from logger import logger
+import streamlit as st
 
 
-settings: Settings = get_settings()
+#env_vars: Envs = Envs.get_envs()
+env_vars: Settings = get_envs()
+
+def page_setup():
+    st.set_page_config(page_title="NoBinge", layout="wide", page_icon="🐧")
+    st.header("NoBinge")
+    
+    if env_vars.GEMINI_API_KEY=="GEMINI_API_KEY":
+        st.sidebar.error("Gemini API Key needed in the env...", icon="ℹ️")
+        st.stop()
+
 
 if __name__ == "__main__":
     logger.info('############NoBinge#############')
-    logger.info(settings)
+    logger.info(env_vars)
+
+
+    #page_setup()
+    
